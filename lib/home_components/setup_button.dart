71cdd064dc2fb3038add49ccbb939307e8ac6d58
ItSeldomRains <agmatthewarapista@gmail.com>
2022-02-06 16:25:44 +0800

    Created selection button (setup_button) in home page

---
+++ lib/home_components/setup_button.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class setup_button extends StatefulWidget {
+  setup_button({Key? key}) : super(key: key);
+
+  @override
+  State<setup_button> createState() => _setup_buttonState();
+}
+
+class _setup_buttonState extends State<setup_button> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: InkWell(
+          onTap: null,
+          child: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.red[100],
+                  borderRadius: BorderRadius.circular(30.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    )
+                  ]),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30.0),
+                child: Container(
+                  child: Container(
+                    height: 150,
+                    width: 300,
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 200,
+                          width: 250,
+                          child: Center(
+                            child: Text(
+                              "SETUP\nWORKOUTS",
+                              style: TextStyle(
+                                  fontFamily: 'Roboto',
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.red[100],
+                            borderRadius: BorderRadius.only(
+                                bottomRight: Radius.circular(100)),
+                          ),
+                        ),
+                        Container()
+                      ],
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
+                        )
+                      ]),
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
