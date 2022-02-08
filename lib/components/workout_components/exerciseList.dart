310d7ea912d66b2c353073a06a7d9b6ac194ef64
ItSeldomRains <agmatthewarapista@gmail.com>
2022-02-08 20:44:43 +0800

    Created list of exercises

---
+++ lib/components/workout_components/exerciseList.dart
@@ -0,0 +1,22 @@
+import 'exerciseItem.dart';
+
+Map<String, Exercise> Exercises = {
+  "dumbell_curl": Exercise(
+      exercise_image: 'assets/img/card_dumbellcurl.png',
+      exercise_name: "dumbell_curl",
+      exercise_displayName: "Dumbell curl",
+      reps: 1,
+      sets: 1),
+  "lateral_raise": Exercise(
+      exercise_image: 'assets/img/card_lateralraise.png',
+      exercise_name: "lateral_raise",
+      exercise_displayName: "Front lateral raise",
+      reps: 1,
+      sets: 1),
+  "shoulder_press": Exercise(
+      exercise_image: 'assets/img/card_shoulderpress.png',
+      exercise_name: "shoulder_press",
+      exercise_displayName: "Shoulder press",
+      reps: 1,
+      sets: 1),
+};
