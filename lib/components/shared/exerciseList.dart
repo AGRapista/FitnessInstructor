8a6ed6ea711a1f21be10c163166f7bb84a4bf8fb
ItSeldomRains <agmatthewarapista@gmail.com>
2022-02-23 16:04:48 +0800

    Moved exerciseList.dart and exerciseItem.dart to shared folder

--- lib/components/shared/exerciseList.dart
+++ lib/components/shared/exerciseList.dart
@@ -1,4 +1,5 @@
 import 'exerciseItem.dart';
+import 'exercise_handler.dart';
 
 Map<String, Exercise> Exercises = {
   "dumbell_curl": Exercise(
@@ -6,17 +7,20 @@ Map<String, Exercise> Exercises = {
       exercise_name: "dumbell_curl",
       exercise_displayName: "Dumbell curl",
       reps: 1,
-      sets: 1),
+      sets: 1,
+      handler: DumbellCurlHandler()),
   "lateral_raise": Exercise(
       exercise_image: 'assets/img/card_lateralraise.png',
       exercise_name: "lateral_raise",
       exercise_displayName: "Front lateral raise",
       reps: 1,
-      sets: 1),
+      sets: 1,
+      handler: DumbellCurlHandler()),
   "shoulder_press": Exercise(
       exercise_image: 'assets/img/card_shoulderpress.png',
       exercise_name: "shoulder_press",
       exercise_displayName: "Shoulder press",
       reps: 1,
-      sets: 1),
+      sets: 1,
+      handler: DumbellCurlHandler()),
 };
